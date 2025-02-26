87501bc1f83e4e18d0eb0964292a9e37de2da954
Omer <113857370+OmerGS@users.noreply.github.com>
2025-02-26 16:26:17 +0100

    feat/PWA Improved PWA icons and manifest

--- application/src/app/layout.tsx
+++ application/src/app/layout.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import Head from "next/head";
 import ServiceWorker from "@/components/reusable/ServiceWorker";
 
 export const metadata = {
   title: "ACTQ",
-  description: "Üyeler için bir plateforme",
+  description: "Üyeler için bir plateform",
   icons: {
     icon: "/icons/icon-192x192.png",
     apple: "/icons/icon-192x192.png",
@@ -15,7 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="fr">
       <head>
-        <meta name="description" content="Üyeler için bir plateforme" />
+        <meta name="description" content="Üyeler için bir plateform" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
