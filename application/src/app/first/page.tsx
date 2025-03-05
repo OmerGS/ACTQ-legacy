c2ea671afc95b0c5bde2f6a373658068a3a44aae
Omer GS <113857370+OmerGS@users.noreply.github.com>
2025-03-05 22:16:54 +0100

    fix/logout route changed

--- application/src/app/first/page.tsx
+++ application/src/app/first/page.tsx
@@ -29,11 +29,6 @@ export default function First() {
   const [showLogoutButton, setShowLogoutButton] = useState(true);
 
   useEffect(() => {
-    if(membre === null || membre.email != null){
-      setMembre(null);
-      return;
-    }
-    
     const checkAuthentication = async () => {
       try {
         const response = await axios.get(`${BACKEND_API.baseURL}/auth/signingMe`, { withCredentials: true });
@@ -133,32 +128,6 @@ export default function First() {
 
     setStep(step + 1);
   };
-
-  if (!membre) {
-    return (
-      <div>
-        <p style={{ textAlign: "center", marginTop: "50px", fontSize: "24px", fontWeight: "bold" }}>
-          404 - İzinsiz giriş
-        </p>
-        <button
-          onClick={() => router.push("/")}
-          style={{
-            display: "block",
-            margin: "20px auto",
-            padding: "10px 20px",
-            fontSize: "16px",
-            cursor: "pointer",
-            backgroundColor: "#4682B4", 
-            color: "#fff", 
-            border: "none", 
-            borderRadius: "5px", 
-          }}
-        >
-          Ana ekrana geri dön
-        </button>
-      </div>
-    );
-  }
 
   return (
     <div style={styles.container}>
@@ -381,7 +350,7 @@ export default function First() {
                   style={styles.buttonContinue}
                   onClick={async () => {
                     try {
-                      const response = await axios.post(`${BACKEND_API.baseURL}/auth/logout`, {}, {
+                      const response = await axios.post(`${BACKEND_API.baseURL}/auth/littleCookieLogout`, {}, {
                         withCredentials: true
                       });
                 
@@ -404,7 +373,7 @@ export default function First() {
                 style={styles.buttonLogout}
                 onClick={async () => {
                   try {
-                    const response = await axios.post(`${BACKEND_API.baseURL}/auth/logout`, {}, {
+                    const response = await axios.post(`${BACKEND_API.baseURL}/auth/littleCookieLogout`, {}, {
                       withCredentials: true
                     });
               
